6de9b9d9fe4e5402e199163eb75e13328c4b4c2c
Ziah <twumasiziah@gmal.com>
2026-07-23 11:54:14 +0000

    adding all files

--- src/atm_prompt.js
+++ src/atm_prompt.js
@@ -5,14 +5,37 @@ const receiptTitle = document.getElementById("receipt-title");
 const receiptText = document.getElementById("receipt-text");
 
 // 1. SET THEINITIAL BALANCE & SECURITY DATA
+let userBalance = 25000;
+let userPin = "1234";
 
 
 // 2. ASK THE USER FOR INPUT (Runs automatically as soon as the page loads)
-
+let enteredPin = prompt("Welcome! Please enter your 4-digit pin: ");
 
 // 3. VALIDATE / CHECK THE PIN NUMBER
 
-    
+if (enteredPin === userPin) {
+     let userAmount = prompt(`PIN ACCEPTED! Your current balance is ${userBalance}. Enter amount to withdraw: `);
+    console.log(typeof userAmount); // string
+    userAmount = Number(userAmount); // reassigning the userAmount variable
+    console.log(typeof userAmount); // number
+    if (userBalance >= userAmount) {
+        let newUserBalance = userBalance - userAmount
+        alert("TRANSACTION SUCCESSFUL");
+        receiptText.innerText = `Dispensing cash... You withdrew ${userAmount}.
+        Your remaining balance is now ${newUserBalance}.`;
+    } else {
+        receiptText.innerText = `Transaction Declined... You cannot withdraw ${userAmount} because your account balance is ${userBalance}.`;
+    }
+} else {
+    receiptText.innerText = "ACCESS DENIED";
+    receiptText.innerText = `Invalid security PIN code entered. Account balance is hidden.`;
+}
+
+
+   
+
+   
     
     
     
@@ -24,4 +47,3 @@ const receiptText = document.getElementById("receipt-text");
 
     
     // TASK 6: Handle the error layout if the PIN was completely wrong
-    
